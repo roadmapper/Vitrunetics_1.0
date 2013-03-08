0bef1a064d2914107ecc2ddc63db68ab2861024a
Vinay Dandekar <vindansam@hotmail.com>
2013-03-08 03:09:01 -0500

    Slowly adding exercises. The table views are actually done, but I wanted to wait until Jonathon finished the final dropdown functionality.

---
+++ Vitrunetics/Models/Exercise.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Vitrunetics.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        public int Sets { get; set; }
+
+        public int Reps { get; set; }
+
+        public int HoldTime { get; set; }
+
+        [DataType(DataType.Text)]
+        public string Comments { get; set; }
+
+    }
+
+    public static class ModelIntializer2
+    {
+        public static List<Exercise> CreateExercises()
+        {
+            return new List<Exercise>
+                       {
+                           new Exercise
+                               {
+                                   Id = 1,
+                                   //Blog = "http://foobar.com",
+                                   Name = "test",
+                                   //Password = "asdfsd",
+                                   //DOB = DateTime.Now.AddYears(1)
+                               }
+                       };
+        }
+    }
+
+    public static class ModelExtention2
+    {
+        public static Exercise Get(this List<Exercise> models, int id)
+        {
+            return models.First(x => x.Id == id);
+        }
+    }
+}
---
+++ Vitrunetics/Models/PatientExerciseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vitrunetics.Models
+{
+    public class PatientExerciseViewModel
+    {
+        public List<Patient> PatientData { get; set; }
+        public List<Exercise> ExerciseData { get; set; }
+
+    }
+}
