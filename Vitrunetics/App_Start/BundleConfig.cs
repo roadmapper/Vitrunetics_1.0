7300ecd892a2a2f3da7d047cf27bda1d497a38b1
Jonathon Blonchek <jblonchek@gmail.com>
2013-03-08 23:28:11 -0500

    Menu Fixed. imported new js bundle and bootstrap jquery files.

--- Vitrunetics/App_Start/BundleConfig.cs
+++ Vitrunetics/App_Start/BundleConfig.cs
@@ -17,7 +17,14 @@ namespace Vitrunetics
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
-
+             bundles.Add(new ScriptBundle("~/js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-migrate-{version}.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/jquery.validate.js",
+                "~/scripts/jquery.validate.unobtrusive.js",
+                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
+                ));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
